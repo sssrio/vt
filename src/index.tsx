a7c45f6692351f9729b1425d3300ef2264df4a7c
sssrio <shuhaila@springrain.io>
2023-09-19 17:26:57 +0600

    fix(skeleton): add credentials

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,28 @@ export function multiply(a: number, b: number): Promise<number> {
 }
 
 export async function ask(age: number, question: string): Promise<string> {
-  const prompt: string = `${question}. Answer this question in a way that a ${age} years old can understand.`;
+  const prompt: string = JSON.stringify({
+    model: process.env.ENGINE || "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: `${question}. Answer this question in a way that a ${age} years old can understand.`,
+      },
+    ],
+  });
   return axios
-    .post('https://testmyreactapplicationfordemopurpose.com', prompt)
-    .then((res) => res.data.result)
-    .catch(() => {
+    .post("https://api.openai.com/v1/chat/completions", prompt, {
+      headers: {
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        "Content-Type": "application/json",
+      },
+    })
+    .then((res) => {
+      const result : string = res.data.choices[0].message.content;
+      return result.replace(/\n\n/g, "\n");
+    })
+    .catch((e) => {
+      console.log("error generating response", e.message)
       return '';
     });
 }

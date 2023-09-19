4a7a0e17ab9388fc155a970234d73eaeeed87a4d
sssrio <shuhaila@springrain.io>
2023-09-20 01:04:51 +0600

    Feature: initialization

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,41 @@
-const axios = require('axios');
-const {ENGINE, OPENAI_API_KEY} = require("@env");
+import axios from 'axios';
 
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
-}
+var studenAge:number = 0;
+var openAiApiKey:string = "";
 
-export async function ask(age: number, question: string): Promise<string> {
-  const prompt: string = JSON.stringify({
-    model: ENGINE || "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content: `${question}. Answer this question in a way that a ${age} years old can understand.`,
-      },
-    ],
-  });
-  return axios
-    .post("https://api.openai.com/v1/chat/completions", prompt, {
-      headers: {
-        Authorization: `Bearer ${OPENAI_API_KEY}`,
-        "Content-Type": "application/json",
-      },
-    })
-    .then((res: any) => {
-      const result : string = res.data.choices[0].message.content;
-      return result.replace(/\n\n/g, "\n");
-    })
-    .catch((e: any) => {
-      console.log("error generating response", e.message)
-      return '';
+export default (apiKey: string, age: number) => {
+  if(!apiKey || !age){
+    throw new Error("Initialization failed! Please check documentation")
+  }
+  studenAge = age;
+  openAiApiKey = apiKey;
+  const ask = async (question: string): Promise<string> => {
+    const prompt: string = JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: `${question}. Answer this question in a way that a ${studenAge} years old can understand.`,
+        },
+      ],
     });
+    return await axios
+      .post("https://api.openai.com/v1/chat/completions", prompt, {
+        headers: {
+          Authorization: `Bearer ${openAiApiKey}`,
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res: any) => {
+        const result : string = res.data.choices[0].message.content;
+        return result.replace(/\n\n/g, "\n");
+      })
+      .catch((e: any) => {
+        console.log("\x1b[43m","\x1b[34m","\x1b[1m",":: VirtualTeacher :: ","\x1b[0m","\n\t\x1b[31m","\x1b[1m","error generating response,", e.message, "\x1b[0m")
+        return '';
+      });
+  }
+  return {
+    ask
+  }
 }

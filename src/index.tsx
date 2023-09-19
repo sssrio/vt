6931899d30b54c7c95aeab88c23a17c5a4a91cba
sssrio <shuhaila@springrain.io>
2023-09-19 17:09:28 +0600

    fix(skeleton): export ask function

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,12 @@
+import axios from "axios";
+
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
+}
+
+export async function ask(age: number, question: string): Promise<string> {
+  const prompt:string = `${question}. Answer this question in a way that a ${age} years old can understand.`;
+  return axios.post("https://testmyreactapplicationfordemopurpose.com", prompt).then(res=>res.data.result).catch(()=>{
+    return "";
+  });
 }
